ae6cf96e2a8f2c3c5037f3d503b51970f19c6c37
David Kurilla <Kurilla.David@student.greenriver.edu>
2024-01-08 15:40:19 -0800

    Week 01 Progress: Basic Springboot project configuration and added Chat class.

---
+++ src/main/java/edu/greenriver/sdev/chatapp/ChatappApplication.java
@@ -0,0 +1,17 @@
+// PACKAGE INFO
+package edu.greenriver.sdev.chatapp;
+
+// IMPORTS
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// CLASS: ChatappApplication
+@SpringBootApplication
+public class ChatappApplication {
+
+	// METHOD: main
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappApplication.class, args);
+	}
+
+}
